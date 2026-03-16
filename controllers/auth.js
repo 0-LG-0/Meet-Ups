509c89dcedb2b6b5dd68ffe2ff9b1bd4396ec2c8
0-LG-0 <luigigjurashaj@gmail.com>
2026-03-16 15:33:14 -0400

    setup sign-in and sign-out

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
 router.get('/sign-up', (req, res) => {
-    res.render('auth/sign-up.ejs')
+    res.render('auth/sign-up.ejs', { message: '' })
 })
 
 router.post('/sign-up', async (req, res) => {
@@ -19,6 +19,10 @@ router.post('/sign-up', async (req, res) => {
 
         if (password !== confirmPassword) {
             throw new Error(`Invalid Password`)
+        }
+
+        if (username.charAt(0) !== username.charAt(0).toUpperCase()) {
+            throw new Error(`The first letter in ${username} must be capitalized`)
         }
 
         const hashedPassword = bcrypt.hashSync(password, 8)
@@ -37,6 +41,40 @@ router.post('/sign-up', async (req, res) => {
     } catch (error) {
         res.render('auth/sign-up.ejs', { message: error.message })
     }
+})
+
+router.get('/sign-in', (req, res) => {
+    res.render('auth/sign-in.ejs', { message: '' })
+})
+
+router.post('/sign-in', async (req,res) => {
+    const { username, password } = req.body 
+    try {
+       
+        const checkUser = await User.findOne({ username })
+        if (!checkUser) {
+            throw new Error (`Invalid Username ${username}`)
+        }
+        const checkPassword = bcrypt.compareSync(password, checkUser.hashedPassword)
+        if (!checkPassword) {
+            throw new Error('Invalid Password')
+        }
+        req.session.user = {
+            _id: checkUser._id,
+            username: checkUser.username,
+        }
+        req.session.save(()=> {
+            res.redirect('/')
+        })
+    } catch (error) {
+        res.render('auth/sign-in.ejs', { message: error.message })
+    }
+})
+
+router.get('/sign-out', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/')
+    })
 })
 
 module.exports = router
